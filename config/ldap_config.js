664335cf76c7501ef1684a8842d467e6ae0d9d51
Sean McGary <sean.mcgary@gmail.com>
2012-01-17 20:10:47 -0500

    makin shit work yo

---
+++ config/ldap_config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: seanmcgary
+ * Date: 1/17/12
+ * Time: 8:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+exports.config = {
+    host: 'ldap://ldap.csh.rit.edu',
+    version: 3,
+    username: '',
+    password: ''
+}
